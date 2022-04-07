8be0f897d1824427b781dca2c0cc82341b06f6a6
Edom Belayneh <edomybbanti@gmail.com>
2022-04-07 05:51:35 -0700

    task 4 fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#!/bin/bash
-
-   /**
-    * main - Entry point
-    *
-    * Return: Always 0 (Success)
-    */
+#include <studio.h>
+/**
+ *main -Entry point
+ *
+ *Return: 0 after printing the function
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle")
-  return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }

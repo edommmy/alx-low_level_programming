a013efaf7655fec1d99ae89e68d37e835a0d9583
Edom Belayneh <edomybbanti@gmail.com>
2022-04-08 09:15:40 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *printing base 10 numbers starting from 0
+ *Return: Always 0
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+printf("%d", num);
+
+printf('\n');
+
+return (0);
+}

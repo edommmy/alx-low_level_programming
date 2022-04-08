731011634382e090a3af05a7297ef94c1b39c0a9
Edom Belayneh <edomybbanti@gmail.com>
2022-04-08 08:49:59 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *lowercase first and then uppercase
+ *Return: 0 Always
+ */
+int main(void)
+{
+int ch;
+int CH;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch)
+}
+for (CH = 'A'; CH <= 'Z'; CH++
+{
+putchar(CH)
+}
+putchar('\n');
+return (0);
+}

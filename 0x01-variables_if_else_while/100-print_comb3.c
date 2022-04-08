7a761cdac4e8058b9681d83b881c85a9f16b374c
Edom Belayneh <edomybbanti@gmail.com>
2022-04-08 11:02:34 -0700

    task 10 fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,9 +7,9 @@
 int main(void)
 {
 int dgt1, dgt2;
-for (dgt1 = 0; dgt1 < 9, dgt1++)
+for (dgt1 = 0; dgt1 < 9; dgt1++)
 {
-for (dgt2 = dgt1 + 1; dgt2 <10; dgt2++)
+for (dgt2 = dgt1 + 1; dgt2 < 10; dgt2++)
 {
 putchar((dgt1 % 10) + '0');
 putchar((dgt2 % 10) + '0');
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,6 +15,6 @@ continue;
 putchar(',');
 putchar(' ');
 }
-putchar('\n');
+putchar ('\n');
 return(0);
 }

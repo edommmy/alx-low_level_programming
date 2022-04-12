fe450a97fa4e14a2b85be52f59c4f347ff2b0f09
Edom Belayneh <edomybbanti@gmail.com>
2022-04-12 06:35:25 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - adds two given integers
+ * @bb: print int b
+ * @gg: print int g
+ * Return: 0 Always
+ */
+int add(int bb, int gg)
+{
+  return (bb + gg);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ int b;
 int r;
 for (a = 0; a <= 9; a++)
 {
-for (b = 0; b <= 9; b++);
+for (b = 0; b <= 9; b++)
 {
 r = a * b;
 if ((r / 10) == 0)
@@ -23,7 +23,7 @@ _putchar ('0');
 if (b != 0)
 {
 _putchar (' ');
-_putchar ((c % 10) + '0');
+_putchar ((r % 10) + '0');
 }
 if (b < 9)
 {
@@ -33,8 +33,8 @@ _putchar (' ');
 }
 else
 {
-_putchar ((c / 10) + '0');
-_putchar ((c % 10) + '0');
+_putchar ((r / 10) + '0');
+_putchar ((r % 10) + '0');
 if (b > 9)
 {
 _putchar (',');

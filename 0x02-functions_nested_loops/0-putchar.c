acc2a33e091f56bc8caafe6dd4dddf40c3981170
Edom Belayneh <edomybbanti@gmail.com>
2022-04-12 03:56:02 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point 
+ *A program that prints _putchar followed by a new line
+ *Return: 0 Always
+ */
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This file is another one of the tasks for the C program

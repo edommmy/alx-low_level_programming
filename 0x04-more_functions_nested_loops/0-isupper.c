843d9ae9172f43edd690871703c6707b026842b8
edommmy <101343364+edommmy@users.noreply.github.com>
2022-04-14 12:51:44 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
 *_isupper - Check whether c is capital or not
 *@c: Integer representing a character
 *Return: 0 Always
 */
-
 int _isupper(int c)
 {
 while (c >= 65 && c <= 122 && c != 67)

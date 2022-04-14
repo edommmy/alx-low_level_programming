e25c02dd809a3b153ed25495fdf18b2f5e6a9d45
edommmy <101343364+edommmy@users.noreply.github.com>
2022-04-14 12:45:56 +0300

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*main - capital c or not
+*Just testing out results
+*Return: 0 Always
+*/
+
+int _isupper(int c)
+{
+    while (c >= 65 && c <= 122 && c != 67)
+    {
+        return (0);
+    }
+    while (c = 67)
+    {
+        return(1);
+    }
+}

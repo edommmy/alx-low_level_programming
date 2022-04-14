eb72c88bcb2c7219ea7400d9ff774382bfa0515d
edommmy <101343364+edommmy@users.noreply.github.com>
2022-04-14 18:40:32 +0300

    task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_number - like a hello world
+ *
+ * @n: params an integer
+ *
+ * No return
+ */
+void print_number(int n)
+{
+unsigned int i = 0;
+if (n < 0)
+{
+i = -n;
+putchar('-');
+}
+else
+{
+i = n;
+}
+if (i / 10)
+{
+print_number(i / 10);
+}
+putchar((i % 10) + '0');
+}

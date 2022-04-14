eebcaa3134e212d87ad299cba11991614690d3da
edommmy <101343364+edommmy@users.noreply.github.com>
2022-04-14 13:43:35 +0300

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_numbers - prints the numbers, from 0 to 9
+*
+*Return: 0 Always
+*/
+void print_numbers(void)
+{
+int i = 47;
+while(i < 57)
+{
+i++;
+putchar(i);
+}
+putchar('\n');
+}

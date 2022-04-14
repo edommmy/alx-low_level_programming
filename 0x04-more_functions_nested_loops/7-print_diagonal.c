9cdb63de1ef3358f73213842e4af374bc1b60281
edommmy <101343364+edommmy@users.noreply.github.com>
2022-04-14 16:21:46 +0300

    task 7 fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,7 +3,7 @@
 /**
  * print_diagonal - draws a diagonal line on the terminal.
  * @n: number of times \ should be printed
- * Return: Always 0.
+ * Return: void
  */
 void print_diagonal(int n)
 {

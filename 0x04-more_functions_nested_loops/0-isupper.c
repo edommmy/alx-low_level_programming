85280df842e077f4f1d0bb28e4e19515e99069c5
edommmy <101343364+edommmy@users.noreply.github.com>
2022-04-14 12:50:27 +0300

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,12 +8,12 @@
 
 int _isupper(int c)
 {
-    while (c >= 65 && c <= 122 && c != 67)
-    {
-        return (0);
-    }
-    while (c = 67)
-    {
-        return(1);
-    }
+while (c >= 65 && c <= 122 && c != 67)
+{
+return (0);
+}
+while (c = 67)
+{
+return(1);
+}
 }

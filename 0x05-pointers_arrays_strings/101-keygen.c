7e7e578dc783c2c8d360459eac443ceff9070581
edommmy <101343364+edommmy@users.noreply.github.com>
2022-04-18 20:25:48 +0300

    Task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+char a[100];
+int r, p, q;
+p = 0;
+q = 0;
+srand(time(NULL));
+while (p < 2645)
+{
+r = rand() % 122;
+if (r > 32)
+{
+a[q++] = r;
+p += r;
+}
+}
+a[q++] = (2772 - p);
+a[q] = '\0';
+printf("%s", a);
+return (0);
+}

2b8aedf6410a0a1a0b462ab28658753c16aecb40
edommmy <101343364+edommmy@users.noreply.github.com>
2022-04-18 20:11:09 +0300

    fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,20 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 /**
- *print_array - prints n elements of an array
- *@a: pointed to an int to be changed
- *@n: return value
- *Return: 0 Always
+ * print_array - prints n elements of an array of integer
+ * @a: A pointed to an int to be changed
+ * @n: return value n
+ * Return: Always 0
  */
 void print_array(int *a, int n)
 {
-  short c = 0;
-  while (n-- > 0)
-    {
-      printf("%d", a[c++]);
-      if (n != 0)
-	{
-	  printf(", ");
-	}
-      printf("\n");
-    }
+int i;
+i = 0;
+while (i < n)
+{
+printf("%d", a[i]);
+if (i < n - 1)
+{
+printf(", ");
+}
+i++;
+}
+printf("\n");
 }

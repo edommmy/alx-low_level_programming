ed9c7f6a4684694beea31124d51eab2e871fc9e7
Edom Belayneh <edomybbanti@gmail.com>
2022-04-18 10:08:57 -0700

    Task 8 fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,16 +7,14 @@
  */
 void print_array(int *a, int n)
 {
-int i;
-i = 0;
-while (i < n)
-{
-printf("%d", a[i]);
-if (i < n - 1);
-{
-printf(", ");
-}
-i++;
-}
-printf("\n");
+  short c = 0;
+  while (n-- > 0)
+    {
+      printf("%d", a[c++]);
+      if (n != 0)
+	{
+	  printf(", ");
+	}
+      printf("\n");
+    }
 }

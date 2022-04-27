df24fe5cc1a10f6a37ad2c637a77517028bc8640
edommmy <101343364+edommmy@users.noreply.github.com>
2022-04-27 19:41:19 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
  *_strlen_recursion -  returns the length of a string
- *@s - string
- *
- *Return: 1 On Success
+ *@s - pointer to string parameter
+ *Return: recursion
  *On error, -1 is returned, and errno is set appropriately
  */
 int _strlen_recursion(char *s)

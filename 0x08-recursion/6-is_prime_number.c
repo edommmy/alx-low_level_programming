622cb1bb649da6fb56a489c4084dbeb7f9143683
edommmy <101343364+edommmy@users.noreply.github.com>
2022-04-27 21:00:50 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,17 +1,5 @@
 #include "main.h"
 /**
- *_prime_number - returns 1 if the input integer is a prime number
- *@n - tested int
- *Return: 1 On succes and 0 if not prime
- */
-int is_prime_number(int n)
-{
-if (n <= 1)
-return (0);
-else
-return (pmnum(n, 2);
-}
-/**
  *pmnum - makes possible to evaluate from 1 to n
  *@a: input int
  *@b: counting var
@@ -25,4 +13,16 @@ if (a % b == 0)
 return (0);
 else
 return (pmnum(a, b + 1));
+}
+/**
+ *_prime_number - returns 1 if the input integer is a prime number
+ *@n - tested int
+ *Return: 1 On succes and 0 if not prime
+ */
+int is_prime_number(int n)
+{
+if (n <= 1)
+return (0);
+else
+return (pmnum(n, 2));
 }

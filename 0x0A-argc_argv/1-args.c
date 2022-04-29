ac780ea3b7ad04b19428c093b7d4c80c1d537983
edommmy <101343364+edommmy@users.noreply.github.com>
2022-04-29 10:45:55 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,9 +1,15 @@
 #include "main.h"
+/**
+ * main - Entry point
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char **argv)
 {
   if (argc > 1)
     {
-      printf("%d\n", argc);
+      printf("%i\n", argc);
     }
   return (0);
 }

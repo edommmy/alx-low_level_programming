40dc85ba768c80fed043fe4b29ba4a49c061176b
edommmy <101343364+edommmy@users.noreply.github.com>
2022-04-29 11:14:25 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,16 +1,21 @@
 #include "main.h"
+/**
+ * main - Entry point
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char **argv)
 {
-  int a;
-  int b;
-  int r;
-  r = a * b;
-  printf("%i\n", r);
-  if (argc < 2)
+  if (argc < 3)
     {
-      char e = 'Error';
-      printf("%s\n", e);
+      printf("%s\n", "Error");
       return (1);
     }
+  else
+  {
+    n = atoi(argv[1]) * atoi(argv[2]);
+    printf("%i\n", n);
   return (0);
+  }
 }

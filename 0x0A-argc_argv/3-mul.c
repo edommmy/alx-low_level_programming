90fb7c1f59b75eaa8c39e40c276c9a7649bb88ea
edommmy <101343364+edommmy@users.noreply.github.com>
2022-04-29 11:15:40 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,15 +7,16 @@
  */
 int main(int argc, char **argv)
 {
-  if (argc < 3)
-    {
-      printf("%s\n", "Error");
-      return (1);
-    }
-  else
-  {
-    n = atoi(argv[1]) * atoi(argv[2]);
-    printf("%i\n", n);
-  return (0);
-  }
+int n;
+if (argc < 3)
+{
+printf("%s\n", "Error");
+return (1);
+}
+else
+{
+n = atoi(argv[1]) * atoi(argv[2]);
+printf("%i\n", n);
+return (0);
+}
 }

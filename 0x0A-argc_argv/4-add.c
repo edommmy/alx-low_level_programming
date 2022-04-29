5059aec035b634eb28f415ce0cc3c65b6a5885c6
Edom Belayneh <edomybbanti@gmail.com>
2022-04-29 01:34:35 -0700

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+int main(int argc, char **argv)
+{
+  int n;
+  if ()
+    {
+      printf("%i\n", 0);
+    }
+  else if ()
+    {
+      printf("%s\n", "Error");
+      return (1);
+    }
+  esle
+    {
+      n = atoi(argv[1]) * atoi(argv[2]);
+      printf("%i\n", n);
+    }
+  return (0);
+}

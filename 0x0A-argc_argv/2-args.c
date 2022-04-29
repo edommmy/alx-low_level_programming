1eb8800e219aa8df6885c977115ffa426d99f0e8
edommmy <101343364+edommmy@users.noreply.github.com>
2022-04-29 10:51:54 +0300

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,19 @@
 #include "main.h"
+/**
+ * main - Entry point
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char **argv)
 {
-  int c
+int n;
+if (argc > 0)
+{
+for (n = 1; n < argc; n++)
+{
+printf("%s\n", argv[n]);
+}
+}
+return (0);
+}

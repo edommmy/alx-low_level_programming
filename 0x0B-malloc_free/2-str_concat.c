dd469edf2ccf235f7064a3924b67e24b484729c4
edommmy <101343364+edommmy@users.noreply.github.com>
2022-05-02 12:01:08 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,7 +7,27 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-  char *s;
-  int x = 0, y = 0, z = 0;
-  if (s1 != NULL
-      
+char *s;
+int x = 0, y = 0, z = 0;
+if (s1 != NULL)
+for (; s1[x]; x++)
+;
+if (s2 != NULL)
+for (; s2[y]; y++)
+;
+s = malloc(sizeof(char) * (x + y + 1));
+if (s == NULL)
+return (NULL);
+while (z < i)
+{
+s[z] = s1[z];
+k++;
+}
+while (z < x + y)
+{
+s[z] = s2[z - x];
+z++;
+}
+s[z] = '\0';
+return (s);
+}

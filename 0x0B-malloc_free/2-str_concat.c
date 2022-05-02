42f20e7ce7caed1887926e844903b8e209e33f8d
Edom Belayneh <edomybbanti@gmail.com>
2022-05-02 01:55:43 -0700

    Task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *str_concat - concatenates two strings
+ *@s1: string 1
+ *@s2: string 2
+ *Return: Pointer On success and NULL on failure
+ */
+char *str_concat(char *s1, char *s2)
+{
+  char *s;
+  int x = 0, y = 0, z = 0;
+  if (s1 != NULL
+      

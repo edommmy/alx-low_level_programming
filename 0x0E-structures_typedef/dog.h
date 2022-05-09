c16b586603e4e64597d35e0a6d1c0ae37b493225
edommmy <101343364+edommmy@users.noreply.github.com>
2022-05-09 17:40:27 +0400

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef STRUCTS
-#define STRUCTS
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - description for a pet
  * @name: pet name
@@ -22,4 +22,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+#endif /* DOG_H */
